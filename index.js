46ab778d262436b4374cc4d509db5fa9c8f39dda
Gabriel Riveros <velo02.gr@gmail.com>
2024-10-31 21:22:01 -0300

    Instalar dependencias nodemon para visualizar los cambios en vivo y configuracion del app listen URL

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // importar dependencias instaladas
 const mongoose = require('mongoose');
+const app = require('./app.js');
 
 // Obtener variables de entorno (esto obtiene del archivo .env pero con la dependencia dotenv)
 require('dotenv').config();
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
+const apiVersion = process.env.API_VERSION;
+const ipServer = process.env.IP_SERVER;
+const port = 3977;
 
 // Ver las variables de entorno
-console.log(`${dbUser} \n${dbPass} \n${dbHost}`);
+// console.log(`${dbUser} \n${dbPass} \n${dbHost} \n${apiVersion} \n${ipServer}`);
 
 // Realizar la conexión
 const connectDB = async () => {
@@ -17,6 +21,12 @@ const connectDB = async () => {
         // conexión con la url del MongoDB
         await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/`);
 
+        app.listen(port, () => {
+            console.log('***********************************')
+            console.log('**************** API **************')
+            console.log('***********************************')
+            console.log(`http://${ipServer}:${port}/api/${apiVersion}`);
+        })
         // Mensaje de conexión exitosa
         console.log('La conexión a la base de datos ha sido satisfactoria');
 
