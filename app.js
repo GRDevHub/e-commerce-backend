5728f56dd2be7751df4e1331747c51e8c22b2300
Gabriel Riveros <velo02.gr@gmail.com>
2024-11-02 12:03:04 -0300

    Agregar configuraciones de Cors, body-parse y static-folder

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-// importamos express
+// Importaciones
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+// Configuraciones (CORS, BODY-PARSER, RUTAS, STATIC-FOLDER)
 
+// * cors
+app.use(cors());
+
+// * body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// * static folder
+app.use(express.static('uploads'));
+
+
+// * Rutas
 
 
 module.exports = app;
